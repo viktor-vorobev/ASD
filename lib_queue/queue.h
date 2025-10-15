a07ed8a17af0a1cecab64a6d9d01059fc337f9f6
vorobev veektor <vorobevveektor20@gmail.com>
2025-10-15 11:42:02 +0300

    add comments

--- lib_queue/queue.h
+++ lib_queue/queue.h
@@ -5,37 +5,39 @@ template<typename T>
 class Queue {
 private:
     struct Node {
-        T data;
-        Node* next;
+        T data;        
+        Node* next;    
+
         Node(const T& value) : data(value), next(nullptr) {}
     };
 
-    Node* frontNode;
-    Node* rearNode;
-    size_t queueSize;
+    Node* frontNode;  // Указатель на начало очереди (первый элемент)
+    Node* rearNode;   // Указатель на конец очереди (последний элемент)
+    size_t queueSize; // Текущий размер очереди
 
 public:
     // Конструкторы и деструктор
-    Queue();
-    Queue(const Queue& other);
-    Queue& operator=(const Queue& other);
-    ~Queue();
+    Queue();                           
+    Queue(const Queue& other);         
+    Queue& operator=(const Queue& other); 
+    ~Queue();                          
 
-    // Основные операции
-    void enqueue(const T& value);
-    void dequeue();
-    T& front();
-    const T& front() const;
-    bool empty() const;
-    size_t size() const;
+    // Основные операции очереди
+    void enqueue(const T& value);      // Добавление элемента в конец очереди
+    void dequeue();                    // Удаление элемента из начала очереди
+    T& front();                        // Получение ссылки на первый элемент (неконстантная версия)
+    const T& front() const;            // Получение ссылки на первый элемент (константная версия)
+    bool empty() const;                // Проверка, пуста ли очередь
+    size_t size() const;               // Получение текущего размера очереди
 
     // Дополнительные методы
-    void clear();
-    void display() const;
+    void clear();                      // Очистка всей очереди
+    void display() const;              // Вывод содержимого очереди (для отладки)
 };
 
 // Явная инстанциация шаблона для распространенных типов
 template class Queue<int>;
 template class Queue<double>;
+
 
 #endif // QUEUE_H
