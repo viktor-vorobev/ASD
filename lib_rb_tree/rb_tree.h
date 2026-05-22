c37252f4c859eed770c2f3cfa5f5623506ae2f41
viktor-vorobev <vorobevveektor20@gmail.com>
2026-05-23 00:32:07 +0300

    rb tree implementation

--- lib_rb_tree/rb_tree.h
+++ lib_rb_tree/rb_tree.h
@@ -3,15 +3,8 @@
 
 #include <iostream>
 #include <functional>
-
-/*
-    RB Tree Properties:
-    1. Узел RED или BLACK.
-    2. Корень BLACK.
-    3. Листья (NIL) BLACK.
-    4. У RED узла оба потомка BLACK.
-    5. Все пути от узла к листьям содержат одинаковое количество BLACK узлов.
-*/
+#include <stdexcept>
+#include <utility>
 
 enum Color { RED, BLACK };
 
@@ -19,41 +12,78 @@ template <typename K, typename V>
 class RBTree {
 private:
     struct Node {
-        K key; V val; Color color;
-        Node* left; Node* right; Node* parent;
-        Node(K k, V v) : key(k), val(v), color(RED), left(nullptr), right(nullptr), parent(nullptr) {}
+        K key;
+        V val;
+        Color color;
+        Node* left;
+        Node* right;
+        Node* parent;
+
+        Node(const K& k, const V& v)
+            : key(k), val(v), color(RED), left(nullptr), right(nullptr), parent(nullptr) {
+        }
+
+        Node(K&& k, V&& v)
+            : key(std::move(k)), val(std::move(v)), color(RED), left(nullptr), right(nullptr), parent(nullptr) {
+        }
     };
+
     Node* root = nullptr;
     int _size = 0;
 
-    void rotateLeft(Node*& root, Node*& pt) {
+    void clearHelper(Node* n) {
+        if (!n) return;
+        clearHelper(n->left);
+        clearHelper(n->right);
+        delete n;
+    }
+
+    Node* copyHelper(Node* n, Node* parentNode) {
+        if (!n) return nullptr;
+        Node* newNode = new Node(n->key, n->val);
+        newNode->color = n->color;
+        newNode->parent = parentNode;
+        try {
+            newNode->left = copyHelper(n->left, newNode);
+            newNode->right = copyHelper(n->right, newNode);
+        }
+        catch (...) {
+            clearHelper(newNode);
+            throw;
+        }
+        return newNode;
+    }
+
+    // Обрати внимание: теперь pt передается по значению (Node*), а не по ссылке, 
+    // чтобы не было проблем с передачей временных указателей при удалении.
+    void rotateLeft(Node*& rootNode, Node* pt) {
         Node* pt_right = pt->right;
         pt->right = pt_right->left;
         if (pt->right != nullptr) pt->right->parent = pt;
         pt_right->parent = pt->parent;
-        if (pt->parent == nullptr) root = pt_right;
+        if (pt->parent == nullptr) rootNode = pt_right;
         else if (pt == pt->parent->left) pt->parent->left = pt_right;
         else pt->parent->right = pt_right;
         pt_right->left = pt;
         pt->parent = pt_right;
     }
 
-    void rotateRight(Node*& root, Node*& pt) {
+    void rotateRight(Node*& rootNode, Node* pt) {
         Node* pt_left = pt->left;
         pt->left = pt_left->right;
         if (pt->left != nullptr) pt->left->parent = pt;
         pt_left->parent = pt->parent;
-        if (pt->parent == nullptr) root = pt_left;
+        if (pt->parent == nullptr) rootNode = pt_left;
         else if (pt == pt->parent->left) pt->parent->left = pt_left;
         else pt->parent->right = pt_left;
         pt_left->right = pt;
         pt->parent = pt_left;
     }
 
-    void fixInsert(Node*& root, Node*& pt) {
+    void fixInsert(Node*& rootNode, Node* pt) {
         Node* parent_pt = nullptr;
         Node* grand_parent_pt = nullptr;
-        while ((pt != root) && (pt->color != BLACK) && (pt->parent->color == RED)) {
+        while ((pt != rootNode) && (pt->color != BLACK) && (pt->parent->color == RED)) {
             parent_pt = pt->parent;
             grand_parent_pt = pt->parent->parent;
             if (parent_pt == grand_parent_pt->left) {
@@ -66,11 +96,11 @@ private:
                 }
                 else {
                     if (pt == parent_pt->right) {
-                        rotateLeft(root, parent_pt);
+                        rotateLeft(rootNode, parent_pt);
                         pt = parent_pt;
                         parent_pt = pt->parent;
                     }
-                    rotateRight(root, grand_parent_pt);
+                    rotateRight(rootNode, grand_parent_pt);
                     std::swap(parent_pt->color, grand_parent_pt->color);
                     pt = parent_pt;
                 }
@@ -85,34 +115,115 @@ private:
                 }
                 else {
                     if (pt == parent_pt->left) {
-                        rotateRight(root, parent_pt);
+                        rotateRight(rootNode, parent_pt);
                         pt = parent_pt;
                         parent_pt = pt->parent;
                     }
-                    rotateLeft(root, grand_parent_pt);
+                    rotateLeft(rootNode, grand_parent_pt);
                     std::swap(parent_pt->color, grand_parent_pt->color);
                     pt = parent_pt;
                 }
             }
         }
-        root->color = BLACK;
+        rootNode->color = BLACK;
     }
 
-    // Для экономии места реализована только вставка (базовый вариант "хорошо"). 
-    // Метод erase выбрасывает исключение как нереализованный.
-
-    Node* BSTInsert(Node* root, Node* pt) {
-        if (root == nullptr) return pt;
-        if (pt->key < root->key) {
-            root->left = BSTInsert(root->left, pt);
-            root->left->parent = root;
+    Node* BSTInsert(Node* rootNode, Node* pt) {
+        if (rootNode == nullptr) return pt;
+        if (pt->key < rootNode->key) {
+            rootNode->left = BSTInsert(rootNode->left, pt);
+            rootNode->left->parent = rootNode;
         }
-        else if (pt->key > root->key) {
-            root->right = BSTInsert(root->right, pt);
-            root->right->parent = root;
+        else if (pt->key > rootNode->key) {
+            rootNode->right = BSTInsert(rootNode->right, pt);
+            rootNode->right->parent = rootNode;
         }
-        return root;
+        return rootNode;
     }
+
+    // --- НОВЫЙ БЛОК ДЛЯ УДАЛЕНИЯ ---
+
+    Node* minimum(Node* node) {
+        while (node->left != nullptr) node = node->left;
+        return node;
+    }
+
+    // Пересадка поддерева: заменяет поддерево с корнем u на поддерево с корнем v
+    void transplant(Node*& rootNode, Node* u, Node* v) {
+        if (u->parent == nullptr) rootNode = v;
+        else if (u == u->parent->left) u->parent->left = v;
+        else u->parent->right = v;
+        if (v != nullptr) v->parent = u->parent;
+    }
+
+    // Балансировка после удаления (восстановление черной высоты)
+    void fixDelete(Node* x, Node* x_parent) {
+        while (x != root && (x == nullptr || x->color == BLACK)) {
+            if (x == x_parent->left) {
+                Node* w = x_parent->right;
+                if (!w) break; // Защита от поврежденной структуры
+                if (w->color == RED) {
+                    w->color = BLACK;
+                    x_parent->color = RED;
+                    rotateLeft(root, x_parent);
+                    w = x_parent->right;
+                    if (!w) break;
+                }
+                if ((w->left == nullptr || w->left->color == BLACK) &&
+                    (w->right == nullptr || w->right->color == BLACK)) {
+                    w->color = RED;
+                    x = x_parent;
+                    x_parent = x->parent;
+                }
+                else {
+                    if (w->right == nullptr || w->right->color == BLACK) {
+                        if (w->left != nullptr) w->left->color = BLACK;
+                        w->color = RED;
+                        rotateRight(root, w);
+                        w = x_parent->right;
+                    }
+                    w->color = x_parent->color;
+                    x_parent->color = BLACK;
+                    if (w->right != nullptr) w->right->color = BLACK;
+                    rotateLeft(root, x_parent);
+                    x = root;
+                }
+            }
+            else {
+                Node* w = x_parent->left;
+                if (!w) break;
+                if (w->color == RED) {
+                    w->color = BLACK;
+                    x_parent->color = RED;
+                    rotateRight(root, x_parent);
+                    w = x_parent->left;
+                    if (!w) break;
+                }
+                if ((w->right == nullptr || w->right->color == BLACK) &&
+                    (w->left == nullptr || w->left->color == BLACK)) {
+                    w->color = RED;
+                    x = x_parent;
+                    x_parent = x->parent;
+                }
+                else {
+                    if (w->left == nullptr || w->left->color == BLACK) {
+                        if (w->right != nullptr) w->right->color = BLACK;
+                        w->color = RED;
+                        rotateLeft(root, w);
+                        w = x_parent->left;
+                    }
+                    w->color = x_parent->color;
+                    x_parent->color = BLACK;
+                    if (w->left != nullptr) w->left->color = BLACK;
+                    rotateRight(root, x_parent);
+                    x = root;
+                }
+            }
+        }
+        if (x != nullptr) x->color = BLACK;
+    }
+
+    // --- КОНЕЦ НОВОГО БЛОКА ---
 
     void inOrderHelper(Node* n, std::function<void(const K&, const V&)> fn) const {
         if (!n) return;
@@ -127,20 +238,40 @@ private:
         return findNode(n->right, key);
     }
 
-    void printTreeHelper(Node* root, std::string indent, bool last) const {
-        if (root != nullptr) {
-            std::cout << indent;
-            if (last) { std::cout << "R----"; indent += "     "; }
-            else { std::cout << "L----"; indent += "|    "; }
-            // ANSI Цвета: 31=Red, 30=Black
-            std::string sColor = root->color == RED ? "\033[31mRED\033[0m" : "\033[30;1mBLACK\033[0m";
-            std::cout << root->key << "(" << sColor << ")" << std::endl;
-            printTreeHelper(root->left, indent, false);
-            printTreeHelper(root->right, indent, true);
-        }
+public:
+    RBTree() = default;
+    ~RBTree() { clear(); }
+
+    RBTree(const RBTree& other) {
+        root = copyHelper(other.root, nullptr);
+        _size = other._size;
     }
 
-public:
+    RBTree& operator=(const RBTree& other) {
+        if (this != &other) {
+            clear();
+            root = copyHelper(other.root, nullptr);
+            _size = other._size;
+        }
+        return *this;
+    }
+
+    RBTree(RBTree&& other) noexcept : root(other.root), _size(other._size) {
+        other.root = nullptr;
+        other._size = 0;
+    }
+
+    RBTree& operator=(RBTree&& other) noexcept {
+        if (this != &other) {
+            clear();
+            root = other.root;
+            _size = other._size;
+            other.root = nullptr;
+            other._size = 0;
+        }
+        return *this;
+    }
+
     bool insert(const K& key, const V& val) {
         if (findNode(root, key)) return false;
         Node* pt = new Node(key, val);
@@ -149,14 +280,88 @@ public:
         _size++;
         return true;
     }
-    bool erase(const K& key) { throw std::runtime_error("RB Erase not fully implemented"); }
+
+    bool insert(K&& key, V&& val) {
+        if (findNode(root, key)) return false;
+        Node* pt = new Node(std::move(key), std::move(val));
+        root = BSTInsert(root, pt);
+        fixInsert(root, pt);
+        _size++;
+        return true;
+    }
+
+    // ПОЛНОЦЕННОЕ БЕЗОПАСНОЕ УДАЛЕНИЕ
+    bool erase(const K& key) {
+        Node* z = root;
+        // 1. Поиск узла
+        while (z != nullptr) {
+            if (key < z->key) z = z->left;
+            else if (key > z->key) z = z->right;
+            else break;
+        }
+        if (z == nullptr) return false; // Ключ не найден
+
+        Node* y = z;
+        Color y_original_color = y->color;
+        Node* x = nullptr;
+        Node* x_parent = nullptr; // Критически важно, если x равен nullptr
+
+        // 2. Отсоединение и замена (Transplant)
+        if (z->left == nullptr) {
+            x = z->right;
+            x_parent = z->parent;
+            transplant(root, z, z->right);
+        }
+        else if (z->right == nullptr) {
+            x = z->left;
+            x_parent = z->parent;
+            transplant(root, z, z->left);
+        }
+        else {
+            y = minimum(z->right);
+            y_original_color = y->color;
+            x = y->right;
+            if (y->parent == z) {
+                x_parent = y;
+            }
+            else {
+                x_parent = y->parent;
+                transplant(root, y, y->right);
+                y->right = z->right;
+                y->right->parent = y;
+            }
+            transplant(root, z, y);
+            y->left = z->left;
+            y->left->parent = y;
+            y->color = z->color;
+        }
+
+        // 3. Безопасное уничтожение памяти
+        delete z;
+        _size--;
+
+        // 4. Восстановление свойств КЧ-дерева, если удалили черный узел
+        if (y_original_color == BLACK) {
+            fixDelete(x, x_parent);
+        }
+        return true;
+    }
+
     V* find(const K& key) const {
         Node* res = findNode(root, key);
         return res ? &(res->val) : nullptr;
     }
+
     int size() const { return _size; }
-    void clear() { root = nullptr; _size = 0; } // Упрощенно
+    bool empty() const { return _size == 0; }
+
+    void clear() {
+        clearHelper(root);
+        root = nullptr;
+        _size = 0;
+    }
+
     void traverseInOrder(std::function<void(const K&, const V&)> fn) const { inOrderHelper(root, fn); }
-    void printColors() const { printTreeHelper(root, "", true); }
 };
-#endif
+
+#endif // RB_TREE_H
--- tests/test_rb_tree.cpp
+++ tests/test_rb_tree.cpp
@@ -1,15 +1,172 @@
 #include <gtest/gtest.h>
-#include "rb_tree.h"
+#include <vector>
 #include <string>
+#include <stdexcept>
+#include "rb_tree.h"
+#include "polynom.h"
 
-TEST(RBTreeTest, InsertAndFind) {
+// БЛОК 1: ТЕСТЫ БАЗОВОЙ ЛОГИКИ RB-ДЕРЕВА
+
+TEST(RBTreeStandardTest, InitialState) {
     RBTree<int, std::string> tree;
-    EXPECT_TRUE(tree.insert(10, "A"));
-    EXPECT_TRUE(tree.insert(20, "B"));
-    EXPECT_TRUE(tree.insert(5, "C"));
+    EXPECT_TRUE(tree.empty());
+    EXPECT_EQ(tree.size(), 0);
+    EXPECT_EQ(tree.find(10), nullptr);
+}
 
+TEST(RBTreeStandardTest, InsertAndTraverse) {
+    RBTree<int, char> tree;
+
+    EXPECT_TRUE(tree.insert(10, 'A'));
+    EXPECT_TRUE(tree.insert(20, 'B'));
+    EXPECT_TRUE(tree.insert(5, 'C'));
+
+    // Дубликаты запрещены
+    EXPECT_FALSE(tree.insert(10, 'X'));
     EXPECT_EQ(tree.size(), 3);
-    auto* val = tree.find(20);
-    ASSERT_NE(val, nullptr);
-    EXPECT_EQ(*val, "B");
+
+    std::vector<int> keys;
+    tree.traverseInOrder([&keys](const int& k, const char& v) {
+        keys.push_back(k);
+        });
+
+    // InOrder обход всегда должен давать отсортированный результат
+    std::vector<int> expected = { 5, 10, 20 };
+    EXPECT_EQ(keys, expected);
+}
+
+TEST(RBTreeStandardTest, RuleOfFiveDeepCopyAndParentPointers) {
+    RBTree<int, int> origin;
+    origin.insert(50, 500);
+    origin.insert(25, 250);
+    origin.insert(75, 750);
+
+    // Тест копирования (copyHelper должен корректно восстановить parent)
+    RBTree<int, int> copied(origin);
+    EXPECT_EQ(copied.size(), origin.size());
+
+    int* valInCopy = copied.find(25);
+    ASSERT_NE(valInCopy, nullptr);
+    *valInCopy = 999;
+
+    // Проверяем независимость копии от оригинала
+    EXPECT_EQ(*origin.find(25), 250);
+
+    // Добавляем элемент в копию, чтобы спровоцировать балансировку.
+    // Если указатели parent были скопированы криво, здесь будет SegFault.
+    EXPECT_TRUE(copied.insert(10, 100));
+    EXPECT_EQ(copied.size(), 4);
+
+    // Тест перемещения (Move Semantics)
+    RBTree<int, int> moved(std::move(origin));
+    EXPECT_EQ(moved.size(), 3);
+    EXPECT_TRUE(origin.empty());
+}
+
+TEST(RBTreeStandardTest, EraseAndMaintainProperties) {
+    RBTree<int, std::string> tree;
+
+    // Вставляем элементы, провоцируя ребалансировку
+    tree.insert(20, "Twenty");
+    tree.insert(10, "Ten");
+    tree.insert(30, "Thirty");
+    tree.insert(5, "Five");
+    tree.insert(15, "Fifteen");
+
+    EXPECT_EQ(tree.size(), 5);
+
+    // Удаляем листовой узел
+    EXPECT_TRUE(tree.erase(5));
+    EXPECT_EQ(tree.find(5), nullptr);
+    EXPECT_EQ(tree.size(), 4);
+
+    // Удаляем узел с одним потомком (или корень)
+    EXPECT_TRUE(tree.erase(20));
+    EXPECT_EQ(tree.find(20), nullptr);
+    EXPECT_EQ(tree.size(), 3);
+
+    // Обход InOrder должен всё ещё возвращать отсортированные данные
+    std::vector<int> expected = { 10, 15, 30 };
+    std::vector<int> actual;
+    tree.traverseInOrder([&actual](const int& k, const std::string& v) {
+        actual.push_back(k);
+        });
+
+    EXPECT_EQ(actual, expected);
+
+    // Удаление несуществующего ключа
+    EXPECT_FALSE(tree.erase(999));
+}
+
+// БЛОК 2: ИНТЕГРАЦИОННЫЕ ТЕСТЫ С POLYNOM
+
+
+TEST(RBTreePolynomIntegration, MoveInsertAndEvaluate) {
+    RBTree<int, Polynom> polyTree;
+
+    // Использование std::move внутри insert()
+    EXPECT_TRUE(polyTree.insert(1, Polynom("x^2 + 2x + 1")));
+    EXPECT_TRUE(polyTree.insert(2, Polynom("5x^3")));
+
+    EXPECT_EQ(polyTree.size(), 2);
+
+    Polynom* p = polyTree.find(1);
+    ASSERT_NE(p, nullptr);
+
+    // Проверка математики: при x=2, "x^2 + 2x + 1" = 4 + 4 + 1 = 9
+    EXPECT_DOUBLE_EQ(p->evaluate(2.0, 0.0, 0.0), 9.0);
+
+    // Модификация (оператор *=)
+    *p *= 2.0; // "2x^2 + 4x + 2"
+    EXPECT_DOUBLE_EQ(p->evaluate(2.0, 0.0, 0.0), 18.0);
+}
+
+TEST(RBTreePolynomIntegration, MemoryLeakPreventionTest) {
+    // Этот тест проверяет каскадное удаление памяти без Double Free.
+    // Ошибки здесь проявятся как падения (Crash) при выходе из скобок.
+    {
+        RBTree<int, Polynom> mainTree;
+        for (int i = 0; i < 10; ++i) {
+            // Динамически вставляем 10 полиномов
+            mainTree.insert(i, Polynom(std::to_string(i) + "x"));
+        }
+
+        EXPECT_EQ(mainTree.size(), 10);
+
+        {
+            // Создаем копию дерева
+            RBTree<int, Polynom> tempTree = mainTree;
+            EXPECT_EQ(tempTree.size(), 10);
+
+            // tempTree выходит из области видимости и вызывает свой деструктор.
+            // Если clearHelper не работает, здесь утечет память.
+        }
+
+        // Оригинальное дерево должно выжить и сохранить данные
+        EXPECT_EQ(mainTree.size(), 10);
+        Polynom* p = mainTree.find(5);
+        ASSERT_NE(p, nullptr);
+        EXPECT_DOUBLE_EQ(p->evaluate(1.0, 0.0, 0.0), 5.0); // "5x" при x=1
+    }
+    // mainTree безопасно уничтожено
+    SUCCEED();
+}
+
+TEST(RBTreePolynomIntegration, ErasePolynomNoLeaks) {
+    RBTree<int, Polynom> tree;
+    tree.insert(1, Polynom("x^2 + x"));
+    tree.insert(2, Polynom("3x"));
+
+    EXPECT_EQ(tree.size(), 2);
+
+    // При вызове erase сработает "delete z" внутри RBTree, 
+    // что вызовет деструктор Polynom и безопасно очистит память List<Monom>.
+    EXPECT_TRUE(tree.erase(1));
+    EXPECT_EQ(tree.size(), 1);
+    EXPECT_EQ(tree.find(1), nullptr);
+
+    // Оставшийся узел не должен повредиться
+    Polynom* p = tree.find(2);
+    ASSERT_NE(p, nullptr);
+    EXPECT_DOUBLE_EQ(p->evaluate(4.0, 0.0, 0.0), 12.0);
 }
